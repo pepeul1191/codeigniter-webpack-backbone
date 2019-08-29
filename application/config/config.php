fdf21b7dc193ebe088b88ad7a0c7c1af36946393
Pepe Valdivia <jvaldivia@softweb.pe>
2019-08-29 18:11:32 -0500

    Agregando config csrf

--- application/config/config.php
+++ application/config/config.php
@@ -521,3 +521,14 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+/*
+|--------------------------------------------------------------------------
+| Custom Configs
+|--------------------------------------------------------------------------
+*/
+
+$config['csrf'] = array(
+  'key' => 'csrf_key',
+  'value' => 'PKBcauXg6sTXz7Ddlty0nejVgoUodXL89KNxcrfwkEme0Huqtj6jjt4fP7v2uF4L', 
+);
---
+++ application/controllers/admin/Login.php
@@ -0,0 +1,37 @@
+<?php
+
+class Login extends CI_Controller
+{
+  public function index()
+  {
+    /*
+    //libraries as filters
+    $this->load->library('ViewSessionFalse', array(
+      'config' => $this->config,
+    ));
+    $this->load->library('HttpAccess',
+      array(
+        'config' => $this->config,
+        'allow' => ['GET'],
+        'received' => $this->input->server('REQUEST_METHOD'),
+        'instance' => $this,
+      )
+    );
+    //controller function
+    $this->load->helper('Login');
+    $data_top = array(
+      'title' => 'Bienvenido',
+      'csss' => index_css($this->config),
+      'jss' => index_js($this->config),
+      'mensaje' => '',
+    );
+    $this->load->view('layouts/blank_header', $data_top);
+    $this->load->view('login/index');
+    $this->load->view('layouts/blank_footer', array());
+    */
+    echo($this->config->item('csrf')['key']);
+    $this->load->view('admin/login/index');
+  }
+}
+
+?>
