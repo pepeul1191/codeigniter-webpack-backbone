7fa2815960bb141b202086ca72a1ea4463fba622
Pepe Valdivia <jvaldivia@softweb.pe>
2020-03-03 09:51:43 -0500

    llenando db con los excel

---
+++ db/doctors.rb
@@ -0,0 +1,73 @@
+# encoding: utf-8
+require 'sequel'
+
+Sequel::Model.plugin :json_serializer
+
+DB = Sequel.connect('sqlite://coa.db')
+
+class Branch < Sequel::Model(DB[:branches])
+
+end
+
+class Specialism < Sequel::Model(DB[:specialisms])
+  
+end
+
+class Dentist < Sequel::Model(DB[:dentists])
+  
+end
+
+class DentistBranch < Sequel::Model(DB[:dentists_branches])
+  
+end
+
+class DentistSpecialism < Sequel::Model(DB[:dentists_specialisms])
+  
+end
+
+f = File.open('doctors.csv', :encoding => 'UTF-8')
+
+DB.transaction do
+  begin
+    f.each_line do |line|
+      array = line.split(':')
+      apellido_paterno = array[0]
+      apellido_materno = array[1]
+      nombres = array[2]
+      specialism = array[3]
+      cop = array[4]
+      rne = array[5]
+      branch = array[6].strip
+      # generate values
+      name = apellido_paterno + ' ' + apellido_materno + ', ' +  nombres
+      if rne == 'xyz' then  rne = '' end
+      specialism_id = Specialism.where(name: specialism).first.id
+      branch_id = Branch.where(name: branch).first.id
+      image = 'assets/img/default-user.png'
+      # insert to dentists
+      dentist = Dentist.new(
+        :name => name,
+        :cop => cop,
+        :rne => rne,
+        :image => image,
+      )
+      dentist.save
+      dentist_branch = DentistBranch.new(
+        :dentist_id => dentist.id, 
+        :branch_id => branch_id, 
+      )
+      dentist_branch.save
+      dentists_specialism = DentistSpecialism.new(
+        :dentist_id => dentist.id, 
+        :specialism_id => specialism_id, 
+      )
+      dentists_specialism.save
+    end
+    f.close
+  rescue Exception => e
+    Sequel::Rollback
+    puts 'error!'
+    puts e.message
+    puts e.backtrace
+  end
+end
